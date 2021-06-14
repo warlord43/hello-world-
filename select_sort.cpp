ec8560d35538b0dc3626452fcffa0e6d08d41902
Yashas Kirnapure <84733649+warlord43@users.noreply.github.com>
2021-06-14 22:30:06 +0530

    Update select_sort.cpp

--- select_sort.cpp
+++ select_sort.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 using namespace std;
 int  swap(int *px, int *py)
@@ -10,21 +9,26 @@ int  swap(int *px, int *py)
   return 0;
 }
 
-int main(){
-  int name[]={2,83,5,6,35,12,478,-35};
-  int min_idx;
-  for(int i=0;i<8;i++){
+int selec_sort(int num[],int n){
+   int min_idx;
+  for(int i=0;i<n-1;i++){
     min_idx=i;
-    for(int j=i+1;j<8;j++){
-      if(name[i]>name[j]){
+    for(int j=i+1;j<n-1;j++){
+      if(num[i]>num[j]){
         min_idx=j;
     if(min_idx!=i){
-        swap(name[i], name[j]);
+        swap(num[i], num[j]);
     }
     }
   }
   }
-for(int i=0;i<8;i++){
-    cout<<name[i]<<" ";
-  }
-  }
+  for(int i=0;i<n-1;i++){
+  cout<<num[i]<<" ";}
+return 0;
+}
+
+
+int main(){
+  int name[]={2,83,5,6,35,12,478,-35};
+  selec_sort(name,8);
+ }
